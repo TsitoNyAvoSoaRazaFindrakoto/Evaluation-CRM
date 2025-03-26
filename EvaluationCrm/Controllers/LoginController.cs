4edbc2dbd331007e4f4fa7a9dc3bc156a0058d12
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:43:55 +0300

    Update LoginController.cs

--- EvaluationCrm/Controllers/LoginController.cs
+++ EvaluationCrm/Controllers/LoginController.cs
@@ -7,29 +7,29 @@ namespace EvaluationCrm.Controllers;
 
 public class LoginController : Controller
 {
-    private readonly UserService _userService;
+	private readonly UserService _userService;
 
-    public LoginController(UserService userService)
-    {
-        _userService = userService;
-    }
+	public LoginController(UserService userService)
+	{
+		_userService = userService;
+	}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-    [HttpPost]
-    public async Task<IActionResult> Login(string email,string password)
-    {
-        var user = await _userService.Login(email,password);
-        if (user != null)
-        {
-           HttpContext.Session.SetString("currentUser",JsonSerializer.Serialize(user));
-           // var current = HttpContext.Session.GetString("currentUser");
-           // var currentUser = JsonSerializer.Deserialize<User>(current);
-            return RedirectToAction("Index", "Dashboard");
-        }
-        ModelState.AddModelError(string.Empty, "Email invalide Or not exist");
-        return View("Index");
-    }
+	public IActionResult Index()
+	{
+		return View();
+	}
+	[HttpPost]
+	public async Task<IActionResult> Login(string email, string password)
+	{
+		var user = await _userService.Login(email, password);
+		if (user != null)
+		{
+			HttpContext.Session.SetString("currentUser", JsonSerializer.Serialize(user));
+			// var current = HttpContext.Session.GetString("currentUser");
+			// var currentUser = JsonSerializer.Deserialize<User>(current);
+			return RedirectToAction("Index", "Dashboard");
+		}
+		ModelState.AddModelError(string.Empty, "Email invalide Or not exist");
+		return View("Index");
+	}
 }
