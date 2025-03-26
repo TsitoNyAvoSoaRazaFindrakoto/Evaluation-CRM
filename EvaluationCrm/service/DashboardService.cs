d359640f7ba6150292ec0eedb5c6b01f9824fbb2
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:46:46 +0300

    Update DashboardService.cs

--- EvaluationCrm/service/DashboardService.cs
+++ EvaluationCrm/service/DashboardService.cs
@@ -4,17 +4,17 @@ namespace EvaluationCrm.service;
 
 public class DashboardService
 {
-    private readonly HttpClient _httpClient;
+	private readonly HttpClient _httpClient;
 
-    public DashboardService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
+	public DashboardService(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
 
-    public Dashboard? getDashboard()
-    {
-        var dashboard = _httpClient.GetFromJsonAsync<Dashboard>("http://localhost:8080/api/dashboard").Result;
-        return dashboard;
-    }
-    
+	public Dashboard? getDashboard()
+	{
+		var dashboard = _httpClient.GetFromJsonAsync<Dashboard>("http://localhost:8080/api/dashboard").Result;
+		return dashboard;
+	}
+
 }
