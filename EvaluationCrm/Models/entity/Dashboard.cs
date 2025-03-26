64c1effde9c009f7b7a2f7627e9e14722b114b98
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:12:07 +0300

    Update Dashboard.cs

--- EvaluationCrm/Models/entity/Dashboard.cs
+++ EvaluationCrm/Models/entity/Dashboard.cs
@@ -2,10 +2,11 @@
 
 public class Dashboard
 {
-    public IDictionary<string, double> TicketExpensesByCustomer { get; set; }
-    public IDictionary<string, double> LeadExpensesByCustomer { get; set; }
-    public IDictionary<string, double> BudgetByCustomer { get; set; }
-    public double TotalTicketExpenses { get; set; }
-    public double TotalLeadExpenses { get; set; }
-    public double TotalBudget { get; set; }
+	public IDictionary<string, double> TicketExpensesByCustomer { get; set; }
+	public IDictionary<string, double> LeadExpensesByCustomer { get; set; }
+	public IDictionary<string, double> BudgetByCustomer { get; set; }
+	public double TotalTicketExpenses { get; set; }
+	public double TotalLeadExpenses { get; set; }
+	public double TotalBudget { get; set; }
+		
 }
