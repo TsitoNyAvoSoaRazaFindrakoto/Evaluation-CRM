23ec3b2ed41db7ba0f3054a7ae49e3b3fa19c7ff
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:44:14 +0300

    Update ApplicationDbContext.cs

--- EvaluationCrm/Data/ApplicationDbContext.cs
+++ EvaluationCrm/Data/ApplicationDbContext.cs
@@ -6,10 +6,10 @@ namespace EvaluationCrm.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-    
-    public DbSet<Role> Roles { get; set; }
-    
-    //public DbSet<Role> Roles { get; set; }
-    // Ajoute d'autres DbSets pour d'autres entités
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+	public DbSet<Role> Roles { get; set; }
+
+	//public DbSet<Role> Roles { get; set; }
+	// Ajoute d'autres DbSets pour d'autres entités
 }
