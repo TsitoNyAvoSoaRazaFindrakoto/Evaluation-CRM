6217f4ea603fae8460b8a26eb69de1f452fc3581
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:44:40 +0300

    Update CustomerLoginInfo.cs

--- EvaluationCrm/Models/entity/CustomerLoginInfo.cs
+++ EvaluationCrm/Models/entity/CustomerLoginInfo.cs
@@ -5,21 +5,21 @@ namespace EvaluationCrm.Models.entity;
 
 public partial class CustomerLoginInfo
 {
-    public int Id { get; set; }
+	public int Id { get; set; }
 
-    public string? Password { get; set; }
+	public string? Password { get; set; }
 
-    public string? Username { get; set; }
+	public string? Username { get; set; }
 
-    public string? Token { get; set; }
+	public string? Token { get; set; }
 
-    public bool? PasswordSet { get; set; }
+	public bool? PasswordSet { get; set; }
 
-    public virtual ICollection<ContractSetting> ContractSettings { get; set; } = new List<ContractSetting>();
+	public virtual ICollection<ContractSetting> ContractSettings { get; set; } = new List<ContractSetting>();
 
-    public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
+	public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
 
-    public virtual ICollection<LeadSetting> LeadSettings { get; set; } = new List<LeadSetting>();
+	public virtual ICollection<LeadSetting> LeadSettings { get; set; } = new List<LeadSetting>();
 
-    public virtual ICollection<TicketSetting> TicketSettings { get; set; } = new List<TicketSetting>();
+	public virtual ICollection<TicketSetting> TicketSettings { get; set; } = new List<TicketSetting>();
 }
