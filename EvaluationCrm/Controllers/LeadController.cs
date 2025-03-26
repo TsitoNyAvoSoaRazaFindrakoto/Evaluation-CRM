cdc8ea00c6228442f017c2566d994dbd0284ce7b
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:43:51 +0300

    Update LeadController.cs

--- EvaluationCrm/Controllers/LeadController.cs
+++ EvaluationCrm/Controllers/LeadController.cs
@@ -4,25 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EvaluationCrm.Controllers
 {
-    public class LeadController : Controller
-    {
-        private readonly LeadService _leadService;
+	public class LeadController : Controller
+	{
+		private readonly LeadService _leadService;
 
-        public LeadController(LeadService service)
-        {
-            _leadService = service;
-        }
-        public IActionResult Index()
-        {
-            List<Lead>? leads = _leadService.GetLeads();
-            return View(leads);
-        }
+		public LeadController(LeadService service)
+		{
+			_leadService = service;
+		}
+		public IActionResult Index()
+		{
+			List<Lead>? leads = _leadService.GetLeads();
+			return View(leads);
+		}
 
-        public IActionResult Delete(Int16 leadId)
-        {
-            _leadService.DeleteLead(leadId);
-            return RedirectToAction("Index");
-        }
-        
-    }
+		public IActionResult Delete(Int16 leadId)
+		{
+			_leadService.DeleteLead(leadId);
+			return RedirectToAction("Index");
+		}
+
+	}
 }
