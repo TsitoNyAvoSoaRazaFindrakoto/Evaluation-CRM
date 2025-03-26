23ba5c239c1c125b1a65ca0de962514afce5ff66
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 13:45:07 +0300

    Update Expense.cs

--- EvaluationCrm/Models/entity/Expense.cs
+++ EvaluationCrm/Models/entity/Expense.cs
@@ -5,20 +5,20 @@ namespace EvaluationCrm.Models.entity;
 
 public partial class Expense
 {
-    public uint ExpenseId { get; set; }
+	public uint ExpenseId { get; set; }
 
-    public decimal Amount { get; set; }
+	public decimal Amount { get; set; }
 
-    public DateOnly DateExpense { get; set; }
+	public DateOnly DateExpense { get; set; }
 
-    public string? Description { get; set; }
+	public string? Description { get; set; }
 
-    public uint? BudgetId { get; set; }
-    
-    public virtual Budget Budget { get; set; } = null;
-    public virtual Customer Customer { get; set; } = null!;
+	public uint? BudgetId { get; set; }
 
-    public virtual ICollection<Lead> TriggerLeads { get; set; } = new List<Lead>();
+	public virtual Budget Budget { get; set; } = null;
+	public virtual Customer Customer { get; set; } = null!;
 
-    public virtual ICollection<Ticket> TriggerTickets { get; set; } = new List<Ticket>();
+	public virtual ICollection<Lead> TriggerLeads { get; set; } = new List<Lead>();
+
+	public virtual ICollection<Ticket> TriggerTickets { get; set; } = new List<Ticket>();
 }
